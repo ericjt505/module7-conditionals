4f41d928819ded56da126f7bd77e515bffb785a5
ericjt505 <weirdspacetobe@gmail.com>
2020-08-18 20:17:11 -0400

    update

--- scripts.js
+++ scripts.js
@@ -1,47 +1,37 @@
+// var select = document.querySelector('select');
+// var para = document.querySelector('p');
+//
+// select .addEventListener('change', setWeather);
+//
+// //using switch statements
+// function setWeather() {
+//   var choice = select.value;
+//
+//   switch (choice) {
+//     case 'sunny':
+//     para.textContent = 'It is nice and sunny out. Go take a walk!';
+//     break;
+//
+//     case 'rainy':
+//     para.textContent = 'Better take an umbrella!';
+//     break;
+//
+//     case 'snowing':
+//     para.textContent = 'Better stay inside with some cocoa.';
+//     break;
+//
+//     case 'overcast':
+//     para.textContent = 'You may want to bring your raincoat just in case';
+//     break;
+//
+//     default:
+//     para.textContent = "";
+//   }
+// }
+
+//ternary operators(condition) ? run this code : run this code instead
+
 var select = document.querySelector('select');
-var para = document.querySelector('p');
+var html = document.querySelector('html');
 
-select .addEventListener('change', setWeather);
-
-function setWeather() {
-  var choice = select.value;
-
-  if(choice==='sunny') {
-    para.textContent = 'It is nice and sunny out. Go talk a walk in the park!'
-  } else if(choice==='rainy') {
-    para.textContent = 'Rain is falling, take an umbrella.'
-  } else if(choice==='snowing') {
-    para.textContent = 'Better stay inside with some cocoa.'
-  } else if(choice==='overcast') {
-    para.textContent= 'You may want to bring your raincoat just in case.'
-  } else {
-    para.textContent = ""
-  }
-}
-
-
-
-
-
-
-
-
-
-/* conditional statements
-
-var choice = BooleanValue (comparison operator)
-
-if (condition) {
-  code to run if condition is true
-} else {
-  runs some other code instead
-}
-
-var shoppingDone = false;
-
-if (shoppingDone === true) {
-  var childsAllowance = 10
-} else {
-  var childsAllowance = 5
-}
-*/
+document.body.style.padding = '100px';

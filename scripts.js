995edc8daa59708107e61ea362b986cef8c8588f
ericjt505 <weirdspacetobe@gmail.com>
2020-08-18 20:17:27 -0400

    update

--- scripts.js
+++ scripts.js
@@ -35,3 +35,14 @@ var select = document.querySelector('select');
 var html = document.querySelector('html');
 
 document.body.style.padding = '100px';
+
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
+
+select.onchange = function(){
+  (select.value === 'black') ?
+  update('black', 'white') :
+  update('white', 'black');
+}
